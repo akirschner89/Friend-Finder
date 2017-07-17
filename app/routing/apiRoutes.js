9fa1b35851765a63b28a180443c627709edf652e
Adam Kirschner <akirschner_atx@Adams-MacBook-Air.local>
2017-07-16 22:20:41 -0500

    pseudo coded new approach

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,16 +17,16 @@ module.exports = function(app) {
             console.log("\n----------------" + "\n scores for each index in friendsData " + friendsData[i].scores);
         };
 
-        // function findMatch(data) {
-        //   for (var i = 0; i < friendsData.length; i++) {
-        //       for (var x = 0; x < 10; x++) {
-        //         newIndexScore = Math.abs(data[x] - friendsData[i].scores[x]);
-        //         console.log(newIndexScore);
-        //       }
-        // };
+// pseudo code for matching
 
-        // findMatch(req.body.scores);
-        // }
+// friendsData[this.length-1].scores for the newewst entry
+
+// for loop from friendsData.length -1 to pull in all the previous objects in the array
+// compare their score from this.legnth-1 score to the other objects before it using Math.abs for loop through each index in each score
+// use a var counter to add each index from math.abs together at the end = total difference
+// match the totaldifference index number that's closest to 0 to the onject in the friendsData array the new entry was matched too
+// include this as the data for the post AJAX call on survey.html to include in the modal
+
         res.json(true);
     });
 };
