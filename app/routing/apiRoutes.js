e1d82ce553cadaaa158fb8514cac9820ee90d013
Adam Kirschner <akirschner_atx@Adams-MacBook-Air.local>
2017-07-16 22:11:53 -0500

    working on matching function

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,12 +3,30 @@ var friendsData = require("../data/friends.js");
 
 module.exports = function(app) {
 
-  app.get("/api/friends", function(req, res) {
-    return res.json(friendsData);
-  });
+    app.get("/api/friends", function(req, res) {
+        return res.json(friendsData);
+    });
 
-  app.post("/api/friends", function(req, res) {
-      friendsData.push(req.body);
-      res.json(true);
-  });
+    app.post("/api/friends", function(req, res) {
+        console.log(req.body);
+        friendsData.push(req.body);
+        console.log("\n----------------" + "\n scores for new entry " + req.body.scores);
+        console.log(friendsData);
+
+        for (var i = 0; i < friendsData.length; i++) {
+            console.log("\n----------------" + "\n scores for each index in friendsData " + friendsData[i].scores);
+        };
+
+        // function findMatch(data) {
+        //   for (var i = 0; i < friendsData.length; i++) {
+        //       for (var x = 0; x < 10; x++) {
+        //         newIndexScore = Math.abs(data[x] - friendsData[i].scores[x]);
+        //         console.log(newIndexScore);
+        //       }
+        // };
+
+        // findMatch(req.body.scores);
+        // }
+        res.json(true);
+    });
 };
