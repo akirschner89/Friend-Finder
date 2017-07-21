1d47145a643e27cf3e6679a5b8a1e8edfd51f42b
Adam Kirschner <akirschner_atx@Adams-MacBook-Air.local>
2017-07-20 22:09:26 -0500

    cleared inputs & selects

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,14 +8,7 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res) {
-        // console.log(req.body);
         friendsData.push(req.body);
-        // console.log("\n----------------" + "\n scores for new entry " + req.body.scores);
-        // console.log(friendsData);
-
-        // for (var i = 0; i < friendsData.length; i++) {
-        //     console.log("\n----------------" + "\n scores for each index in friendsData " + friendsData[i].scores);
-        // };
 
         // pseudo code for finding the best match
 
@@ -79,21 +72,10 @@ module.exports = function(app) {
                 //   targetScore++;
                 // }
                 console.log("new round targetScore: " + targetScore);
-                console.log("new round counter: " + x);
+                console.log("new round index of friends counter: " + x);
                 findScore();
             }
         }
-
-
-
-
-
-
-        // for loop from friendsData.length -1 to pull in all the previous objects in the array
-        // compare their score from this.legnth-1 score to the other objects before it using Math.abs for loop through each index in each score
-        // use a var counter to add each index from math.abs together at the end = total difference
-        // match the totaldifference index number that's closest to 0 to the onject in the friendsData array the new entry was matched too
-        // include this as the data for the post AJAX call on survey.html to include in the modal
 
         res.json(friendsData[x]);
     });
