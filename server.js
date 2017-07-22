2f1196c491c490ffbdf97f978de40f5e647d7c92
Adam Kirschner <akirschner_atx@Adams-MacBook-Air.local>
2017-07-22 15:59:46 -0500

    favicon fix pre-deployment

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 3003;
 var favicon = require("serve-favicon");
-var path = require('path');
+var path = require("path");
 
-app.use(favicon(__dirname + '/app/public/images/favicon.png'));
-app.use(favicon(path.join(__dirname,'app','public','images','favicon.png')));
+// app.use('/app', express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname + "/app/public/dog-16-203766.png")));
+app.use('/app', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
