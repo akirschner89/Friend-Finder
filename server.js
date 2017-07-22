af3af24d86f8a015f0193e9fe615b8aa56515a55
Adam Kirschner <akirschner_atx@Adams-MacBook-Air.local>
2017-07-22 16:37:25 -0500

    files prepped for Heroku

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-var PORT = process.env.PORT || 3003;
+var PORT = process.env.PORT || 3000;
 var favicon = require("serve-favicon");
 var path = require("path");
 
-// app.use('/app', express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname + "/app/public/dog-16-203766.png")));
-app.use('/app', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
@@ -16,6 +14,4 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
-});
+app.listen(PORT);
